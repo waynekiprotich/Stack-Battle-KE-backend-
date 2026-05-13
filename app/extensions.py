d3c02e376c866e8091c66010befd1ec7916224dc
Chris kabue <chris.kabue@student.moringaschool.com>
2026-05-13 09:37:26 +0300

    mofified:   app/extensions.py

--- app/extensions.py
+++ app/extensions.py
@@ -1,10 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 
 # Initialize SQLAlchemy and Migrate with no app bound yet
 db = SQLAlchemy()
 migrate = Migrate()
-bcrypt = Bcrypt()
 jwt = JWTManager()
