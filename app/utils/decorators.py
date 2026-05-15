18daa29184967374b78f586bce0384bc3a59a45a
Chris kabue <chris.kabue@student.moringaschool.com>
2026-05-15 17:17:44 +0300

    add custom decorators

---
+++ app/utils/decorators.py
@@ -0,0 +1,16 @@
+from functools import wraps
+from flask import jsonify
+from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity
+
+
+def jwt_required_user(fn):
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        from app.models.user import User
+        user_id = get_jwt_identity()
+        current_user = User.query.get(user_id)
+        if not current_user:
+            return jsonify({"error": "User not found"}), 404
+        return fn(current_user, *args, **kwargs)
+    return wrapper
